c7fd43e7dc6142448f92d610f850159eb6143cdd
leeniallen <139717385+leeniallen@users.noreply.github.com>
2023-07-21 20:46:04 +0000

    ;lad

--- script.js
+++ script.js
@@ -6,22 +6,36 @@ let backpack = {
     clickValueAdded:1,
     costIncrease: 1.2
 }
+function calculateClickValue(){
+    let clickValue = 1 + backpack.quantity * backpack.clickValueAdded
+    return clickValue
+}
+
 
 function getGold() {
-    let clickValue = 1 + backpack.quantity * backpack.clickValueAdded
+  let clickValue = calculateClickValue()
     gold += clickValue
-    // gold = gold + 1
+    // gold = gold + calculatedClickValue 
     console.log(gold, clickValue)
     draw()
 }
 
 function draw() {
 document.getElementById('gold-count').innerText = gold
+document.getElementById("clickValue").innerText =
+calculateClickValue()
+
 }
 
 function getBackpack(){
     if (gold > backpack.price){
         backpack.quantity = backpack.quantity +1
         gold -= backpack.price
+        backpack.price =  backpack.price * backpack.costIncrease
+        draw()
     }    
+
+else{
+    alert("you don't have enough gold!")
+}
 }
